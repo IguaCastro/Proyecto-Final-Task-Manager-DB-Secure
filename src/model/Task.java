0b943c0536f70a31161efea24ab4f890896f2df3
unknown <miyarmoises2004@gmail.com>
2026-04-11 11:50:47 -0500

    subiendo los atributos de la clase Task

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Task {
-    
+    private int id;
+    private String title;
+    private boolean completed;
+    private int userId;
+
+    public Task(int id, String title, boolean completed, int userId){
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+        this.userId = userId;
+    }
 }
